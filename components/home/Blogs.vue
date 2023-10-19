78d7bdd3796b21e0a491475327f537dc23b9550c
laosong-2020 <songzl0606@gmail.com>
2023-10-19 00:19:58 -0500

    add

---
+++ components/home/Blogs.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import BlogCard from '../blogs/BlogCard.vue';
+</script>
+
+<template>
+  <div class="grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-3 gap-6 lg:gap-4 p-0 md:py-4 md:p-0">
+    <BlogCard />
+    <BlogCard />
+    <BlogCard />
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 const route = useRoute()
-import Hero from '..//components/home/Hero.vue';
-
+import Hero from '../components/home/Hero.vue';
+import Blogs from '../components/home/Blogs.vue';
 definePageMeta({
   layout: 'default',
   scrollToTop: true,
@@ -11,6 +11,6 @@ definePageMeta({
 <template>
   <main class="space-y-4">
     <Hero />
-    <Hero />
+    <Blogs />
   </main>
 </template>
