640f2f8ab5374332ffbd4f7958a05d174705879d
laosong-2020 <songzl0606@gmail.com>
2023-10-22 22:00:28 -0500

    Refactor & Add Experiences - Move components to subdirs. - Add Experience Section. - Add Skill Progress Section.

--- app.config.ts
+++ app.config.ts
@@ -34,5 +34,57 @@ export default defineAppConfig({
       imgSrc: '/logos/TAMULogo.png',
       description: 'GPA: 3.8/4.0',
     }
-  ]
+  ],
+  experience: [
+    {
+      companyName: 'Huawei Technologies',
+      companyLocation: 'Shanghai, China',
+      position: 'Software Development Engineer',
+      date: 'Jun. 2018 - Dec. 2019',
+      imgSrc: '/logos/HuaweiLogo.png',
+      description: '111'
+    },
+    {
+      companyName: 'Sercomm Corporation',
+      companyLocation: 'Suzhou, China',
+      position: 'Software Development Engineer',
+      date: 'Jul. 2017 - Jun. 2018',
+      imgSrc: '/logos/SercommLogo.png',
+      description: '222'
+    },
+    {
+      companyName: 'OmniVision Technologies',
+      companyLocation: 'Shanghai, China',
+      position: 'Software Development Engineer Intern',
+      date: 'Jun. 2016 - Oct. 2016',
+      imgSrc: '/logos/OmniVisionLogo.png',
+      description: '333'
+    }
+  ],
+  skills: [
+    {
+      name: 'Python',
+      level: '80',
+    },
+    {
+      name: 'Linux C/C++',
+      level: '70',
+    },
+    {
+      name: 'Backend Development',
+      level: '60',
+    },
+    {
+      name: 'CI/CD',
+      level: '70',
+    },
+    {
+      name: 'Frontend Development',
+      level: '40',
+    },
+    {
+      name: 'GIS',
+      level: '70',
+    }
+  ],
 })
